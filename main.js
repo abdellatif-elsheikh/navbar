7133725b8d3f1fea7b29718a5087bed5ec40db7a
Abdellatif Elsheikh <70378861+abdellatif-elsheikh@users.noreply.github.com>
2024-03-24 14:40:18 +0200

    make it response to the scroll

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ const showNavMenu = () => {
 }
 
 mobileMenu.addEventListener('click', showNavMenu)
+
+// make the navigation bar interactive with the scroll
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (currentScroll > lastScrollTop) {
+    document.querySelector('.navbar').classList.add('hide');
+  } else {
+    document.querySelector('.navbar').classList.remove('hide');
+  }
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}, false);
